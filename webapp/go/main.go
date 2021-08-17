904305fefd6a96cdf831ec59de15881591d9f74b
anko9801 <daiusa3@icloud.com>
2021-08-17 22:59:02 +0900

    reduce condtions

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -870,8 +870,24 @@ func searchRecommendedEstateWithChair(c echo.Context) error {
 	w := chair.Width
 	h := chair.Height
 	d := chair.Depth
-	query = `SELECT * FROM estate WHERE (door_width >= ? AND door_height >= ?) OR (door_width >= ? AND door_height >= ?) OR (door_width >= ? AND door_height >= ?) OR (door_width >= ? AND door_height >= ?) OR (door_width >= ? AND door_height >= ?) OR (door_width >= ? AND door_height >= ?) ORDER BY popularity DESC, id ASC LIMIT ?`
-	err = db.Select(&estates, query, w, h, w, d, h, w, h, d, d, w, d, h, Limit)
+	var m1, m2 int64
+	if w <= h {
+		m1 = w
+		if h <= d {
+			m2 = h
+		} else {
+			m2 = d
+		}
+	} else {
+		m1 = h
+		if w <= d {
+			m2 = w
+		} else {
+			m2 = d
+		}
+	}
+	query = `SELECT * FROM estate WHERE (door_width >= ? AND door_height >= ?) OR (door_width >= ? AND door_height >= ?) ORDER BY popularity DESC, id ASC LIMIT ?`
+	err = db.Select(&estates, query, m1, m2, m2, m1, Limit)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return c.JSON(http.StatusOK, EstateListResponse{[]Estate{}})
