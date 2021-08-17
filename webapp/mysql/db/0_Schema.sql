2161b2d983bc4199e03b7d406564912e30f62657
anko9801 <daiusa3@icloud.com>
2021-08-18 01:26:29 +0900

    divide table

--- webapp/mysql/db/0_Schema.sql
+++ webapp/mysql/db/0_Schema.sql
@@ -1,29 +1,7 @@
 DROP DATABASE IF EXISTS isuumo;
 CREATE DATABASE isuumo;
 
-DROP TABLE IF EXISTS isuumo.estate;
 DROP TABLE IF EXISTS isuumo.chair;
-
-CREATE TABLE isuumo.estate
-(
-    id          INTEGER             NOT NULL PRIMARY KEY,
-    name        VARCHAR(64)         NOT NULL,
-    description VARCHAR(4096)       NOT NULL,
-    thumbnail   VARCHAR(128)        NOT NULL,
-    address     VARCHAR(128)        NOT NULL,
-    latitude    DOUBLE PRECISION    NOT NULL,
-    longitude   DOUBLE PRECISION    NOT NULL,
-    rent        INTEGER             NOT NULL,
-    door_height INTEGER             NOT NULL,
-    door_width  INTEGER             NOT NULL,
-    features    VARCHAR(64)         NOT NULL,
-    popularity  INTEGER             NOT NULL,
-    INDEX all_index (`door_height`, `door_width`, `rent`, `id`),
-    INDEX nazotte_index (`latitude`, `longitude`, `popularity` DESC, `id` ASC),
-    INDEX union_index (`door_width`, `door_height`, `popularity` DESC, `id` ASC),
-    INDEX rent_index (`rent`, `id`),
-    INDEX rent_popularity (`rent`, `popularity` DESC, `id`)
-) ENGINE=MyISAM;
 
 CREATE TABLE isuumo.chair
 (
