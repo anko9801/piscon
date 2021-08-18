0ca6fb2f5cbd64f35c677b29d4660220bc1985df
anko9801 <daiusa3@icloud.com>
2021-08-18 23:14:19 +0900

    余計な部分をカット

--- webapp/mysql/db/0_Schema.sql
+++ webapp/mysql/db/0_Schema.sql
@@ -5,19 +5,19 @@ DROP TABLE IF EXISTS isuumo.chair;
 
 CREATE TABLE isuumo.chair
 (
-    id          INTEGER         NOT NULL PRIMARY KEY,
-    name        VARCHAR(64)     NOT NULL,
-    description VARCHAR(100)    NOT NULL,
-    thumbnail   VARCHAR(128)    NOT NULL,
-    price       INTEGER         NOT NULL,
-    height      INTEGER         NOT NULL,
-    width       INTEGER         NOT NULL,
-    depth       INTEGER         NOT NULL,
-    color       VARCHAR(64)     NOT NULL,
-    features    VARCHAR(64)     NOT NULL,
-    kind        VARCHAR(64)     NOT NULL,
-    popularity  INTEGER         NOT NULL,
-    stock       INTEGER         NOT NULL,
+    id          INTEGER          NOT NULL PRIMARY KEY,
+    name        VARCHAR(32)      NOT NULL,
+    description VARCHAR(100)     NOT NULL,
+    thumbnail   VARCHAR(100)     NOT NULL,
+    price       INTEGER          NOT NULL,
+    height      TINYINT UNSIGNED NOT NULL,
+    width       TINYINT UNSIGNED NOT NULL,
+    depth       TINYINT UNSIGNED NOT NULL,
+    color       VARCHAR(4)       NOT NULL,
+    features    VARCHAR(32)      NOT NULL,
+    kind        VARCHAR(16)      NOT NULL,
+    popularity  INTEGER          NOT NULL,
+    stock       TINYINT UNSIGNED NOT NULL,
     INDEX all_index (`price`, `height`, `width`, `depth`, `kind`, `color`, `features`, `stock`, `popularity` DESC, `id`),
     INDEX stock_price (`stock`, `price`, `id`)
 ) ENGINE=InnoDB;
