a9bc58f178962a91da25ed1f0930179c1b344f77
yukikurage <55534323+yukikurage@users.noreply.github.com>
2021-08-19 17:32:28 +0900

    make index

--- webapp/mysql/db/01_Schema.sql
+++ webapp/mysql/db/01_Schema.sql
@@ -22,6 +22,6 @@ CREATE TABLE isuumo.estate
     INDEX union_index (`door_width`, `door_height`, `popularity` DESC, `id` ASC),
     INDEX rent_index (`rent`, `id`),
     INDEX rent_popularity (`rent`, `popularity` DESC, `id`),
-    INDEX chair_search_index (`id`, `name`, `description`, `thumbnail`, `address`, `latitude`, `longitude`, `rent`, `door_height`, `door_width`, `features`, `popularity`),
+    INDEX door_width_door_height (`door_width`, `door_height`),
     SPATIAL INDEX (geom)
 ) ENGINE=MyISAM;
