7957498cc1a66658b56bb6c83184e7a34a4065ec
anko9801 <daiusa3@icloud.com>
2021-08-20 12:23:25 +0900

    print debug2

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -666,7 +666,7 @@ func searchChairs(c echo.Context) error {
 	}
 
 	res.Chairs = chairs
-	log.Printf("Chairs %d %d %d\n", res.Count, len(res.Chairs), perPage)
+	c.Echo().Logger.Infof("Chairs %d %d %d\n", res.Count, len(res.Chairs), perPage)
 
 	return c.JSON(http.StatusOK, res)
 }
@@ -924,7 +924,7 @@ func searchEstates(c echo.Context) error {
 	}
 
 	res.Estates = estates
-	log.Printf("Estate %d %d %d\n", res.Count, len(res.Estates), perPage)
+	c.Echo().Logger.Infof("Estate %d %d %d\n", res.Count, len(res.Estates), perPage)
 
 	return c.JSON(http.StatusOK, res)
 }
